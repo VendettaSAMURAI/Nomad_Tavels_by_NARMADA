bc0998cc87713fd10a9573acf92383a2ae074f15
Nomad_Travel <Nomad_Travel@example.com>
2023-09-04 22:15:25 +0530

    first commit

---
+++ Home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+
+    <!-- swiper css link is here -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+    <!-- Font code link -->
+    <link rel="stylesheet" href="<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Custom css File link -->
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+    
+<!-- header starts from here  -->
+
+<Section class="Header">
+    <a href="home.php" class="logo">Travel.</a>
+
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="Book.php">Book</a>
+    </nav>
+
+
+</Section>
+
+<!-- header section ends here -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Custom swiper script is here -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!-- Custom javascript file is here -->
+<script src="js/Script.js"></script>
+</body>
+</html>
